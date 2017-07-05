168b0708620e38033535a676d3b031365a3736ce
yarikonline <yar370@yandex.ru>
2017-07-05 17:45:14 +0500

    learning interface

--- ASGlobalItem.cs
+++ ASGlobalItem.cs
@@ -10,14 +10,14 @@ namespace AmmoSwitcher
         // this changes type of projectile fired... easy
         public override void PickAmmo(Item item, Player player, ref int type, ref float speed, ref int damage, ref float knockback)
         {
-            Item selectedAmmo = player.inventory[player.GetModPlayer<AmmoSwitcherPlayer>(mod).useAmmoSlot];
+            Item selectedAmmo = player.inventory[player.GetModPlayer<ASPlayer>(mod).useAmmoSlot];
             // some ammo have different projectile, then fired from weapon, this must be preserved
             
 
             // change projectule type to selected
             if (selectedAmmo.ammo == item.ammo && player.HeldItem.shoot == 0)
             {
-                type = player.inventory[player.GetModPlayer<AmmoSwitcherPlayer>(mod).useAmmoSlot].shoot;
+                type = player.inventory[player.GetModPlayer<ASPlayer>(mod).useAmmoSlot].shoot;
             }
         }
 
